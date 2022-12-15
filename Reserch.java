c3dc9de81f842ad49b0488c731b74ecb18133381
Marat <email.marat@gmail.com>
2022-12-15 18:04:28 +0300

    First commit

---
+++ Reserch.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.io.*;
+
+
+public class Reserch {
+    ArrayList<String> result = new ArrayList<>();
+    ArrayList<Node> tree;
+
+    public Reserch(GeoTree geoTree) {
+        tree = geoTree.getTree();
+    }
+
+    public ArrayList<String> spend(Person p, Relationship re) {
+        for (Node t : tree) {
+            if (t.p1.fullName == p.fullName && t.re == re) {
+                result.add(t.p2.fullName);
+            }
+        }
+        return result;
+    }
+}
---
+++ WriteFile.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.ArrayList;
+
+
+public class WriteFile {
+        ArrayList<Node> tree;
+        ArrayList<String> result = new ArrayList<>();
+
+    public WriteFile(GeoTree geoTree) {
+        tree = geoTree.getTree();
+        
+    }
+
+    public ArrayList<String> spend() {
+        for (Node t : tree) {      
+            result.add(t.p1.fullName + " " + t.re + " " + t.p2.fullName);
+        }
+    
+        
+        return result;
+    }
+}
